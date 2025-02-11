b0eb63c760d2afbf5e69e0b888289fce95a8ca36
chanufernando <Chanuli2021@gmail.com>
2025-02-11 14:36:13 +0530

    Create hoempage UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from "./Components/Navbar";
 import Feed from "./Components/Feed";
 
-function App() {
+export default function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
@@ -16,15 +12,3 @@ function App() {
     </div>
   );
 }
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>      
-  )
-}
-
-export default App
