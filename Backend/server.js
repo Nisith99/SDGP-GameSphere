57d09777d1119f7bbe3c6b5c8b78fbed980fcf34
KRashmika13 <krashmika13@gmail.com>
2025-02-13 22:17:57 +0530

    update server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -18,3 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(process.env.PORT,() => {
     console.log('Server is running on port 3001');
 });
+
+app.get("/", (req, res) => {
+    res.send();
+} )
