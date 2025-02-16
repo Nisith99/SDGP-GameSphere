c3927be997b673b6223f2a1a42bd3aae36c3ecfe
KRashmika13 <krashmika13@gmail.com>
2025-02-16 23:35:15 +0530

    added login functionality with JWT authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -22,7 +22,7 @@ export const signup =async(req, res) => {
 		});
 		await user.save();
 
-		const token = jwt.sign({id: user_.id}, process.env.JWT_SECRET, {expiresIn: '2d'});
+		const token = jwt.sign({id:user_.id}, process.env.JWT_SECRET, {expiresIn: '2d'});
 		res.cookie('token', token, {
 			httpOnly: true,
 			secure: process.env.NODE_ENV === 'production',
@@ -37,4 +37,34 @@ export const signup =async(req, res) => {
 	console.log("Error in signup: ", error.message);
 	res.status(500).json({message: "internal server error"}); 
    }
-}
+};
+
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		
+		const user = await User.findOne({ email });
+		if (!email) {
+			return res.status(400).json({ message: "Invalid email" });
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return res.status(400).json({ message: "Invalid password" });
+		}
+		
+		const token = jwt.sign({id:user_.id}, process.env.JWT_SECRET, {expiresIn: '2d'});
+		res.cookie('token', token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+			maxAge: 2 * 24 * 60 * 60 * 1000
+		})
+
+		res.json({ message: "Logged in successfully" });
+	} catch (error) {
+		console.error("Error in login:", error);
+		res.status(500).json({ message: "Server error" });
+	}
+};
