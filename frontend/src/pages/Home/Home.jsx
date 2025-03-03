3e6d730e1e1c53fc80ff7818dc996adf46b75af4
chanufernando <Chanuli2021@gmail.com>
2025-03-03 19:18:49 +0530

    Update Home.jsx
    Add availability part to home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import Navbar from "../../Components/Navbar";
 import Feed from "../../Components/Feed"
+import AvailabilityCard from "../../Components/AvailabilityCard";
 
 export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen">
       {/* Navbar */}
       <Navbar />
+      
+      {/* Availability Card - Positioned Below Navbar */}
+      <div className="flex justify-center">
+        <AvailabilityCard />
+      </div>
+      
 
       {/* Main Content */}
       <div className="max-w-2xl mx-auto mt-4 space-y-4">
