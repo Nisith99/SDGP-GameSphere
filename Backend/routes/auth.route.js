d44a8943b9a458c661319f231f0702e466f265d0
chanufernando <Chanuli2021@gmail.com>
2025-03-04 12:55:57 +0530

    Update auth.route.js
    Change lines

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {signup, login, selectRole, getProfile} from "../controllers/auth.controller.js";
+import { signup, login, selectRole, getProfile } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/role", selectRole);
-router.post("/profile", getProfile);
+router.get("/profile", getProfile);
 
 export default router;
