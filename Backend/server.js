0f4205c974b6b77efff6e5e34f43a8e459055f3b
chanufernando <Chanuli2021@gmail.com>
2025-03-06 21:11:28 +0530

    Update server.js
    Change lines

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,48 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import bcrypt from 'bcryptjs';
+import cors from "cors";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
+import { router as postRoutes } from "./routes/postRoutes.js";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import playerRoutes from "./routes/player.route.js";
 import clubRoutes from "./routes/club.route.js";
+import postRoutes from "./routes/postRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-dotenv.config();
-
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+  })
+);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Conncted to database successfuly"))
-.catch(err => console.log("Faild to connect to database",err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to database successfully"))
+  .catch((err) => console.log("Failed to connect to database", err));
 
-app.listen(process.env.PORT,() => {
-    console.log('Server is running on port 3001');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/user", userRoutes)
-app.use("/api/v1/player", playerRoutes)
-app.use("/api/v1/club", clubRoutes)
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/player", playerRoutes);
+app.use("/api/v1/club", clubRoutes);
+app.use("/api/posts", postRoutes);
+
+export default app;
