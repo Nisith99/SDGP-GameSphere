4ebdb680931b46f39c22926b96f2387c6f8bc238
Nisith99 <nisithinduwara@gmail.com>
2025-03-22 10:54:16 +0530

    rating system backend implementation user routes.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,12 +1,20 @@
+// user.route.js
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getSuggestedConnections, getPublicProfile, updateProfile } from "../controllers/user.controller.js";
+import { 
+  getSuggestedConnections, 
+  getPublicProfile, 
+  updateProfile, 
+  rateUser, 
+  getUserRatings 
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/suggestions", protectRoute, getSuggestedConnections);
 router.get("/:username", protectRoute, getPublicProfile);
-
 router.put("/profile", protectRoute, updateProfile);
+router.post("/rate/:userId", protectRoute, rateUser);
+router.get("/ratings/:username", protectRoute, getUserRatings);
 
 export default router;
