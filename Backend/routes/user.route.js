dffb8dba89bb336999832901e86ec81ed03a91bb
KRashmika13 <krashmika13@gmail.com>
2025-03-22 14:17:16 +0530

    upadte user route for search

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getSuggestedConnections, getPublicProfile, updateProfile } from "../controllers/user.controller.js";
+import {
+  getSuggestedConnections,
+  getPublicProfile,
+  updateProfile,
+  searchUsers,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+console.log("user.route.js loaded");
+
 router.get("/suggestions", protectRoute, getSuggestedConnections);
 router.get("/:username", protectRoute, getPublicProfile);
-
 router.put("/profile", protectRoute, updateProfile);
+router.get("/search", protectRoute, (req, res, next) => {
+  console.log("Search endpoint hit with query:", req.query.q);
+  searchUsers(req, res, next);
+});
 
 export default router;
