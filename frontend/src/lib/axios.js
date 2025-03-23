97be5f5bffc736eb97f86ef45d391610ad01053f
Fahad-2025-07 <fahadazhar03072004@gmail.com>
2025-03-23 19:11:58 +0530

    axios upadte

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1", 
+  baseURL: "https://sdgp-gamesphere-deploy-backend-production.up.railway.app/api/v1", 
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
