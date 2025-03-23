5e739a13cd4186925517c2d815551b390c0c1e6c
ishani20030803 <ishanidasunika441@gmail.com>
2025-03-23 11:47:51 +0530

    chat with the players created. (notification.model.js)

--- Backend/models/Notification.model.js
+++ Backend/models/Notification.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const notificationSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    type: { type: String, required: true, enum: ["like", "comment", "connectionAccepted"] },
+    type: { type: String, required: true, enum: ["like", "comment", "connectionAccepted", "message"] },
     relatedUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     relatedPost: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
     read: { type: Boolean, default: false },
