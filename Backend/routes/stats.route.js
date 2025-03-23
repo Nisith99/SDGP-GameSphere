63b26502a23643a3de8db1768a16b5641fb50ce8
Fahad-2025-07 <fahadazhar03072004@gmail.com>
2025-03-23 16:17:50 +0530

    backend stats fix

--- Backend/routes/stats.route.js
+++ Backend/routes/stats.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUserStats, getTestUserStats, addTestUser } from "../controllers/stats.controller.js";
+import { getAllUserStats, getTestUserStats} from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -8,5 +8,9 @@ console.log("stats.route.js loaded");
 
 // Main endpoint to get all user stats - protected by auth middleware
 router.get("/", protectRoute, getAllUserStats);
+
+// Public test endpoint for development
+router.get("/test", getTestUserStats);
+
 
 export default router;
