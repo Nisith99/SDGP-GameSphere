ab7a6f898995f2dc0bf8123cf055e94ef0b578f4
KRashmika13 <krashmika13@gmail.com>
2025-04-08 00:53:21 +0530

    Update cloudinary.js

--- Backend/lib/cloudinary.js
+++ Backend/lib/cloudinary.js
@@ -9,4 +9,16 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+export const uploadToCloudinary = async (file, folder) => {
+	return new Promise((resolve, reject) => {
+	  cloudinary.uploader.upload_stream(
+		{ folder },
+		(error, result) => {
+		  if (error) return reject(error);
+		  resolve(result);
+		}
+	  ).end(file.data); // file.data is a Buffer
+	});
+};
+
 export default cloudinary;
